cdc439a35b1081a71db1567f0c6cc21af6004687
Student <example@example.com>
2020-02-05 08:57:02 -0800

    add lib files

--- app/assets/javascripts/readvideo.js
+++ app/assets/javascripts/readvideo.js
@@ -1,3 +1,20 @@
+function waitForLoad(id, callback){
+    var timer = setInterval(function(){
+        if(document.getElementById(id)){
+            clearInterval(timer);
+            callback();
+        }
+    }, 100);
+}
+
+waitForLoad("subm", function(){
+    console.log("load successful, you can proceed!!");
+    document.getElementById("subm").onclick = function()
+    {
+        alert("I got clicked");
+    }
+});
+
 aalib.read.video.fromVideoElement(document.querySelector("video"))
 
   .pipe(aalib.aa({ width: 165, height: 68 }))
