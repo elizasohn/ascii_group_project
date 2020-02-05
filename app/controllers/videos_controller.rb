053beb6954160b7381e405446ce31aeee961f46f
Student <example@example.com>
2020-02-05 11:07:20 -0800

    comment code, move slideshow into extract_frames

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,18 +12,17 @@ class VideosController < ApplicationController
   def show
     # breaks without @url?
     @url = url_for(@video.clip)
-
-    if @video
-      slideshow_transcoder = FFMPEG::Transcoder.new(
-        '',
-        'app/assets/images/slideshow.mp4', # output
-        { resolution: "320x240" },
-        input: "app/assets/images/frame_%d.jpg",
-        input_options: { framerate: '30' }
-      )
-
-      slideshow = slideshow_transcoder.run
-    end
+    # binding.pry
+    # if @frames
+    # slideshow_transcoder = FFMPEG::Transcoder.new(
+    #   '',
+    #   'app/assets/images/slideshow.mp4', # output
+    #   { resolution: "320x240" },
+    #   input: "app/assets/images/frame_%d.jpg",
+    #   input_options: { framerate: '30' }
+    # )
+    # slideshow = slideshow_transcoder.run
+    # end
   end
 
   # GET /videos/new
@@ -34,7 +33,7 @@ class VideosController < ApplicationController
   # GET /videos/1/edit
   def edit
     @url = url_for(@video.clip)
-    @video.extract_frames(@url)
+    @frames = @video.extract_frames(@url)
     # routes to URL parameter at Video.rb #extract_frames
   end
 
@@ -45,8 +44,9 @@ class VideosController < ApplicationController
       @video = Video.new(video_params)
       respond_to do |format|
         if @video.save
+          # binding.pry
           format.html { redirect_to @video, notice: 'Video was successfully created.' }
-          format.json { render :show, status: :created, location: @video }
+          format.json { render :edit, status: :created, location: @video }
         else
           format.html { render :new }
           format.json { render json: @video.errors, status: :unprocessable_entity }
@@ -58,38 +58,38 @@ class VideosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /videos/1
-  # PATCH/PUT /videos/1.json
-  def update
-    respond_to do |format|
-      if @video.update(video_params)
-        format.html { redirect_to @video, notice: 'Video was successfully updated.' }
-        format.json { render :show, status: :ok, location: @video }
-      else
-        format.html { render :edit }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
-      end
+# PATCH/PUT /videos/1
+# PATCH/PUT /videos/1.json
+def update
+  respond_to do |format|
+    if @video.update(video_params)
+      format.html { redirect_to @video, notice: 'Video was successfully updated.' }
+      format.json { render :show, status: :ok, location: @video }
+    else
+      format.html { render :edit }
+      format.json { render json: @video.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  # DELETE /videos/1
-  # DELETE /videos/1.json
-  def destroy
-    @video.destroy
-    respond_to do |format|
-      format.html { redirect_to videos_url, notice: 'Video was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+# DELETE /videos/1
+# DELETE /videos/1.json
+def destroy
+  @video.destroy
+  respond_to do |format|
+    format.html { redirect_to videos_url, notice: 'Video was successfully destroyed.' }
+    format.json { head :no_content }
   end
+end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_video
-      @video = Video.find(params[:id])
-    end
+private
+# Use callbacks to share common setup or constraints between actions.
+def set_video
+  @video = Video.find(params[:id])
+end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def video_params
-      params.require(:video).permit(:clip)
-    end
+# Never trust parameters from the scary internet, only allow the white list through.
+def video_params
+  params.require(:video).permit(:clip)
+end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -6,15 +6,20 @@ class Video < ApplicationRecord
     puts "made it to extract_frames"
     movie = FFMPEG::Movie.new(url)
     puts "made it past movie declaration"
-    puts movie.duration
-    puts movie.width
-    puts movie.height
-    # movie.screenshot("screenshot.bmp", seek_time: 5, resolution: '320x240')
     movie.screenshot(
-      "app/assets/images/frame_%d.jpg", 
-      { vframes: 20, frame_rate: '1.5' }, 
+      "app/assets/images/frame_%d.jpg",
+      { vframes: 30, frame_rate: '1' },
       validate: false
     )
+
+    slideshow_transcoder = FFMPEG::Transcoder.new(
+      '',
+      'app/assets/images/slideshow.mp4', # output
+      { resolution: "320x240" },
+      input: "app/assets/images/frame_%d.jpg",
+      input_options: { framerate: '30' }
+    )
+    slideshow = slideshow_transcoder.run
   end
 
   private
@@ -29,10 +34,4 @@ class Video < ApplicationRecord
       errors.add(:clip, 'Must have a video attached.')
     end
   end
-
-  # def image_attached
-  #   if clip.attached? == false
-  #     errors.add(:clip, 'Please attach a video, friend.')
-  #   end
-  # end
 end
