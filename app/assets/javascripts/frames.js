967cb1e2843fbe698b47bd11079c6107508dcd4f
Student <example@example.com>
2020-02-06 13:10:34 -0800

    add set interval for fifty frames

--- app/assets/javascripts/frames.js
+++ app/assets/javascripts/frames.js
@@ -1,77 +1,194 @@
 setTimeout(function() {
-    $("#frame1").css('display', 'block');
-    $("#frame2").css('display', 'none');
-    $("#frame3").css('display', 'none');
-    $("#frame4").css('display', 'none');
-    $("#frame5").css('display', 'none');
-    $("#frame6").css('display', 'none');
-    $("#frame7").css('display', 'none');
-    $("#frame8").css('display', 'none');
-    $("#frame9").css('display', 'none');
-    $("#frame10").css('display', 'none');
-    $("#frame11").css('display', 'none');
-    $("#frame12").css('display', 'none');
-    $("#frame13").css('display', 'none');
-    $("#frame14").css('display', 'none');
-    $("#frame15").css('display', 'none');
-
+    $("#frame1","#frame2","#frame3","#frame4","#frame5","#frame6","#frame7","#frame8","#frame9","#frame10","#frame11","#frame12","#frame13","#frame14","#frame15", "#frame16", "#frame17", "#frame18", "#frame19", "#frame20", "#frame21", "#frame22", "#frame23", "#frame24", "#frame25", "#frame26", "#frame27", "#frame28", "#frame29", "#frame30", "#frame31", "#frame33", "#frame34", "#frame35", "#frame36", "#frame37", "#frame38", "#frame39", "#frame40", "#frame41", "#frame42", "#frame43", "#frame44", "#frame45", "#frame46", "#frame47", "#frame48", "#frame49", "#frame50").css('display', 'none');
+  },250);
+  setTimeout(function() {
+    $("#frame1").css('display', 'none');
+    $("#frame2").css('display', 'block');
   },500);
-       setTimeout(function() {
-          $("#frame1").css('display', 'none');
-           $("#frame2").css('display', 'block');
-         },1000);
-         setTimeout(function() {
-           $("#frame2").css('display', 'none');
-           $("#frame3").css('display', 'block');
-         },1500);
-           setTimeout(function() {
-               $("#frame3").css('display', 'none');
-               $("#frame4").css('display', 'block');
-             },2000);
-             setTimeout(function() {
-               $("#frame4").css('display', 'none');
-               $("#frame5").css('display', 'block');
-             },2500);
-             setTimeout(function() {
-                $("#frame5").css('display', 'none');
-                 $("#frame6").css('display', 'block');
-               },3000);
-               setTimeout(function() {
-                 $("#frame6").css('display', 'none');
-                 $("#frame7").css('display', 'block');
-               },3500);
-                 setTimeout(function() {
-                     $("#frame7").css('display', 'none');
-                     $("#frame8").css('display', 'block');
-                   },4000);
-                   setTimeout(function() {
-                     $("#frame8").css('display', 'none');
-                     $("#frame9").css('display', 'block');
-                   },4500);
-                   setTimeout(function() {
-                      $("#frame9").css('display', 'none');
-                       $("#frame10").css('display', 'block');
-                     },5000);
-                     setTimeout(function() {
-                       $("#frame10").css('display', 'none');
-                       $("#frame11").css('display', 'block');
-                     },5500);
-                       setTimeout(function() {
-                           $("#frame11").css('display', 'none');
-                           $("#frame12").css('display', 'block');
-                         },6000);
-                         setTimeout(function() {
-                           $("#frame12").css('display', 'none');
-                           $("#frame13").css('display', 'block');
-                         },6500);
-                         setTimeout(function() {
-                            $("#frame13").css('display', 'none');
-                             $("#frame14").css('display', 'block');
-                           },7000);
-                           setTimeout(function() {
-                             $("#frame14").css('display', 'none');
-                             $("#frame15").css('display', 'block');
-                           },7500);
-                             setTimeout(function() {
-                                 $("#frame15").css('display', 'none');;
-                               },8000);
+  setTimeout(function() {
+    $("#frame2").css('display', 'none');
+    $("#frame3").css('display', 'block');
+  },750);
+  setTimeout(function() {
+    $("#frame3").css('display', 'none');
+    $("#frame4").css('display', 'block');
+  },1000);
+  setTimeout(function() {
+    $("#frame4").css('display', 'none');
+    $("#frame5").css('display', 'block');
+  },1250);
+  setTimeout(function() {
+    $("#frame5").css('display', 'none');
+    $("#frame6").css('display', 'block');
+  },1500);
+  setTimeout(function() {
+    $("#frame6").css('display', 'none');
+    $("#frame7").css('display', 'block');
+  },1750);
+  setTimeout(function() {
+    $("#frame7").css('display', 'none');
+    $("#frame8").css('display', 'block');
+  },2000);
+  setTimeout(function() {
+    $("#frame8").css('display', 'none');
+    $("#frame9").css('display', 'block');
+  },2250);
+  setTimeout(function() {
+    $("#frame9").css('display', 'none');
+    $("#frame10").css('display', 'block');
+  },2500);
+  setTimeout(function() {
+    $("#frame10").css('display', 'none');
+    $("#frame11").css('display', 'block');
+  },2750);
+  setTimeout(function() {
+    $("#frame11").css('display', 'none');
+    $("#frame12").css('display', 'block');
+  },3000);
+  setTimeout(function() {
+    $("#frame12").css('display', 'none');
+    $("#frame13").css('display', 'block');
+  },3250);
+  setTimeout(function() {
+    $("#frame13").css('display', 'none');
+    $("#frame14").css('display', 'block');
+  },3500);
+  setTimeout(function() {
+    $("#frame14").css('display', 'none');
+    $("#frame15").css('display', 'block');
+  },3750);
+  setTimeout(function() {
+    $("#frame15").css('display', 'none');
+    $("#frame16").css('display', 'block');
+  },4000);
+  setTimeout(function() {
+    $("#frame16").css('display', 'none');
+    $("#frame17").css('display', 'block');
+  },4250);
+  setTimeout(function() {
+    $("#frame17").css('display', 'none');
+    $("#frame18").css('display', 'block');
+  },4500);
+  setTimeout(function() {
+    $("#frame18").css('display', 'none');
+    $("#frame19").css('display', 'block');
+  },4750);
+  setTimeout(function() {
+    $("#frame19").css('display', 'none');
+    $("#frame20").css('display', 'block');
+  },5000);
+  setTimeout(function() {
+    $("#frame20").css('display', 'none');
+    $("#frame21").css('display', 'block');
+  },5250);
+  setTimeout(function() {
+    $("#frame21").css('display', 'none');
+    $("#frame22").css('display', 'block');
+  },5500);
+  setTimeout(function() {
+    $("#frame22").css('display', 'none');
+    $("#frame23").css('display', 'block');
+  },5750);
+  setTimeout(function() {
+    $("#frame23").css('display', 'none');
+    $("#frame24").css('display', 'block');
+  },6000);
+  setTimeout(function() {
+    $("#frame24").css('display', 'none');
+    $("#frame25").css('display', 'block');
+  },6250);
+  setTimeout(function() {
+    $("#frame25").css('display', 'none');
+    $("#frame26").css('display', 'block');
+  },6500);
+  setTimeout(function() {
+    $("#frame26").css('display', 'none');
+    $("#frame27").css('display', 'block');
+  },6750);
+  setTimeout(function() {
+    $("#frame27").css('display', 'none');
+    $("#frame28").css('display', 'block');
+  },7000);
+  setTimeout(function() {
+    $("#frame29").css('display', 'none');
+    $("#frame30").css('display', 'block');
+  },7250);
+  setTimeout(function() {
+    $("#frame30").css('display', 'none');
+    $("#frame31").css('display', 'block');
+  },7500);
+  setTimeout(function() {
+    $("#frame31").css('display', 'none');
+    $("#frame32").css('display', 'block');
+  },8750);
+  setTimeout(function() {
+    $("#frame32").css('display', 'none');
+    $("#frame33").css('display', 'block');
+  },9000);
+  setTimeout(function() {
+    $("#frame33").css('display', 'none');
+    $("#frame34").css('display', 'block');
+  },9250);
+  setTimeout(function() {
+    $("#frame34").css('display', 'none');
+    $("#frame35").css('display', 'block');
+  },9500);
+  setTimeout(function() {
+    $("#frame35").css('display', 'none');
+    $("#frame36").css('display', 'block');
+  },9750);
+  setTimeout(function() {
+    $("#frame36").css('display', 'none');
+    $("#frame37").css('display', 'block');
+  },10000);
+  setTimeout(function() {
+    $("#frame37").css('display', 'none');
+    $("#frame38").css('display', 'block');
+  },10250);
+  setTimeout(function() {
+    $("#frame38").css('display', 'none');
+    $("#frame39").css('display', 'block');
+  },10500);
+  setTimeout(function() {
+    $("#frame39").css('display', 'none');
+    $("#frame40").css('display', 'block');
+  },10750);
+  setTimeout(function() {
+    $("#frame40").css('display', 'none');
+    $("#frame41").css('display', 'block');
+  },11000);
+  setTimeout(function() {
+    $("#frame41").css('display', 'none');
+    $("#frame42").css('display', 'block');
+  },11250);
+  setTimeout(function() {
+    $("#frame42").css('display', 'none');
+    $("#frame43").css('display', 'block');
+  },11500);
+  setTimeout(function() {
+    $("#frame43").css('display', 'none');
+    $("#frame44").css('display', 'block');
+  },11750);
+  setTimeout(function() {
+    $("#frame44").css('display', 'none');
+    $("#frame45").css('display', 'block');
+  },12000);
+  setTimeout(function() {
+    $("#frame45").css('display', 'none');
+    $("#frame46").css('display', 'block');
+  },12250);
+  setTimeout(function() {
+    $("#frame46").css('display', 'none');
+    $("#frame47").css('display', 'block');
+  },12500);
+  setTimeout(function() {
+    $("#frame47").css('display', 'none');
+    $("#frame48").css('display', 'block');
+  },12750);
+  setTimeout(function() {
+    $("#frame49").css('display', 'none');
+    $("#frame50").css('display', 'block');
+  },13000);
+  setTimeout(function() {
+    $("#frame50").css('display', 'none');;
+  },13250);
