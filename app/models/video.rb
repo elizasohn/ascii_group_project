8f59f90241e884af01d1fe0f65eb30864022bf37
Student <example@example.com>
2020-02-06 09:51:44 -0800

    add logic to wipe images folder every pass, separate ascii_frames into new assets folder, update gitignore

--- app/models/video.rb
+++ app/models/video.rb
@@ -15,6 +15,11 @@ class Video < ApplicationRecord
       { vframes: frame_captures, frame_rate: duration },
       validate: false
     )
+    movie.screenshot(
+      "app/assets/ascii_frames/ascii_frame_%d.jpg",
+      { vframes: frame_captures, frame_rate: duration },
+      validate: false
+    )
 
     frame_num = 1;
     frame_captures.times {
@@ -30,8 +35,7 @@ class Video < ApplicationRecord
       1.times {
         frames << img.implode(implosion)
         legend.annotate(frames, 0,0,10,20, sprintf("% 4.2f", implosion))
-        # frames.matte = false
-        implosion -= 2.5
+        implosion -= 4
       }
 
       frames.write("app/assets/images/frame_#{frame_num}.jpg")
@@ -52,7 +56,14 @@ class Video < ApplicationRecord
 
     file_deletion_count = directory_count - 1
     file_deletion_count.times {
-      File.delete("app/assets/images/frame_#{file_deletion_count}.jpg") if File.exist?("app/assets/images/frame_#{file_deletion_count}.jpg")}
+      if File.exist?("app/assets/images/frame_#{file_deletion_count}.jpg")
+        puts "frame_#{file_deletion_count}.jpg exists and was removed"
+        File.delete("app/assets/images/frame_#{file_deletion_count}.jpg")
+        file_deletion_count = file_deletion_count - 1
+      else
+        puts "No file"
+      end
+    }
   end
 
   private
